167e0a2a09de738b543cf8bbdffdc6e1c99c830c
dlexion <dlexion@yandex.ru>
2018-04-13 09:04:24 +0300

    Realize state class

--- Mapping/State.cs
+++ Mapping/State.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace Mapping
 {
     class State
     {
+        public List<List<PointF>> Polygons { get; set; }
+        public string Name { get; set; }
+
+        public State(string name, List<List<PointF>> points)
+        {
+            Name = name;
+            Polygons = points;
+        }
     }
 }
