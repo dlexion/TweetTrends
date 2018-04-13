198992559b8eb84a8bf4026cd1129273128539cb
dlexion <dlexion@yandex.ru>
2018-04-13 09:16:13 +0300

    Mapping

--- Mapping/Form1.cs
+++ Mapping/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,41 @@ namespace Mapping
 {
     public partial class Form1 : Form
     {
+        List<State> states;
         public Form1()
         {
             InitializeComponent();
+            states = StatesReader.Read(@"D:\PS3_TweetTrens\trends\data\states1.json");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (var state in states)
+            {
+                foreach (var points in state.Polygons)
+                {
+                    g.DrawPolygon(new Pen(Color.Black, 2), points.ToArray());
+                    g.FillPolygon(Brushes.Blue, points.ToArray());
+
+                }
+            }
+
+            g.Dispose();
+            Console.WriteLine(states.Count);
+
+            Bitmap bitmap = new Bitmap(Convert.ToInt32(1180), Convert.ToInt32(640), PixelFormat.Format32bppArgb);
+            Graphics gr = Graphics.FromImage(bitmap);
+            gr.Clear(Color.White);
+            foreach (var state in states)
+            {
+                foreach (var points in state.Polygons)
+                {
+                    gr.DrawPolygon(new Pen(Color.Black, 2), points.ToArray());
+                    gr.FillPolygon(Brushes.Red, points.ToArray());
+                }
+            }
+            bitmap.Save(@"pic.bmp", ImageFormat.Bmp);
         }
     }
 }
---
+++ Mapping/StatesReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping
+{
+    class StatesReader
+    {
+        public static List<State> Read(string path)
+        {
+            List<State> states = new List<State>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                int i = 0;
+                string name = String.Empty;
+                List<List<PointF>> polygons = new List<List<PointF>>();
+                List<PointF> points = new List<PointF>();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        if ((line = line.Trim(new char[] { ' ', ',' })).StartsWith("\""))
+                        {
+                            if (polygons.Count > 0)
+                            {
+                                i = 0;
+                                states.Add(new State(name, polygons));
+                                polygons = new List<List<PointF>>();
+                                points = new List<PointF>();
+
+                            }
+                            name = line.Trim(new char[] { '\\', '"', '[', ' ', ':' });
+                        }
+                        else
+                        {
+                            float temp = Convert.ToSingle(line.Replace('.', ','));
+                            points.Add(new PointF(temp * 10 + 1800, Convert.ToSingle(line = sr.ReadLine().Trim().Replace('.', ',')) * -10 + 750));
+
+                            if (points[0] == points[points.Count - 1] && points.Count > 1)
+                            {
+                                polygons.Add(points);
+                                points = new List<PointF>();
+                            }
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+                states.Add(new State(name, polygons));
+                return states;
+            }
+        }
+    }
+}
